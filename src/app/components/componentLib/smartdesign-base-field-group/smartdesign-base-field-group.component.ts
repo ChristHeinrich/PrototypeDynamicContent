1ce6d6e41db86e76b609588d09c52c8451b250ba
christian.heinrich <christian.heinrich@cas.de>
2019-02-13 13:47:17 +0100

    recursive reading of dsl works (some stuff deactivated)

---
+++ src/app/components/componentLib/smartdesign-base-field-group/smartdesign-base-field-group.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-smartdesign-base-field-group',
+  templateUrl: './smartdesign-base-field-group.component.html',
+  styleUrls: ['./smartdesign-base-field-group.component.css']
+})
+export class SmartdesignBaseFieldGroupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/componentLib/smartdesign-base-field/smartdesign-base-field.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Directive } from '@angular/core';
+
+@Component({
+  selector: 'smartdesign-base-field',
+  templateUrl: './smartdesign-base-field.component.html',
+  styleUrls: ['./smartdesign-base-field.component.css']
+})
+export class SmartdesignBaseFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
