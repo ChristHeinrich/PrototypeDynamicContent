a511461263167657ec9fd2f90ae98341c4603ce4
christian.heinrich <christian.heinrich@cas.de>
2019-02-21 10:37:19 +0100

    smartdesign-base-link-field

--- src/app/components/componentLib/smartdesign-base-linkfield/smartdesign-base-linkfield.component.ts
+++ src/app/components/componentLib/smartdesign-base-linkfield/smartdesign-base-linkfield.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-smartdesign-base-linkfield',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./smartdesign-base-linkfield.component.css']
 })
 export class SmartdesignBaseLinkfieldComponent implements OnInit {
-
+ @Input() formConfig;
   constructor() { }
 
   ngOnInit() {
